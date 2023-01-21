461c608e949db4e96afaae12b24a0f4cb950ef09
Maxsoon4k <118830315+Buhankd@users.noreply.github.com>
2023-01-21 10:03:41 +0300

    first commit

---
+++ xlib project/Assets/scripts/VuforiaStart.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VuforiaStart : MonoBehaviour
+{
+     public void Vuforia()
+        {
+            SceneManager.LoadScene("vuforia");
+
+        }
+}
